e6f2161c43af1c08f73bc7e676e67ea54428f969
Marek Guminski <marek.guminski@creotech.pl>
2015-03-19 14:15:41 +0100

    Minimal set of changes required to run WR on kintex board

--- include/board-wrc.h
+++ include/board-wrc.h
@@ -14,8 +14,8 @@
 #define CPU_CLOCK 62500000ULL
 
 /* WR Reference clock period (picoseconds) and frequency (Hz) */
-#define REF_CLOCK_PERIOD_PS 8000
-#define REF_CLOCK_FREQ_HZ 125000000
+#define REF_CLOCK_PERIOD_PS 16000
+#define REF_CLOCK_FREQ_HZ 62500000
 
 /* Baud rate of the builtin UART (does not apply to the VUART) */
 #define UART_BAUDRATE 115200ULL
@@ -33,7 +33,7 @@ int board_init();
 int board_update();
 
 /* spll parameter that are board-specific */
-#define BOARD_DIVIDE_DMTD_CLOCKS	1
+#define BOARD_DIVIDE_DMTD_CLOCKS	0
 #define BOARD_MAX_CHAN_REF		1
 #define BOARD_MAX_CHAN_AUX		2
 #define BOARD_MAX_PTRACKERS		1
